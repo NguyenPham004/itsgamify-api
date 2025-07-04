bb29b85cfe2ad88247f707e6baee78010fa52651
JTRerer <trongan147895@gmail.com>
2025-07-04 17:30:16 +0700

    feat: course-participations

--- src/its.gamify.api/Controllers/CourseParticipationsController.cs
+++ src/its.gamify.api/Controllers/CourseParticipationsController.cs
@@ -1,35 +1,30 @@
-// using its.gamify.core.Features.CourseParticipations.Queries;
-// using MediatR;
-// using Microsoft.AspNetCore.Mvc;
+using its.gamify.core.Features.CourseParticipations.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace its.gamify.api.Controllers
-// {
-//     [ApiController]
-//     [Route("api/course-participations")]
-//     public class CourseParticipationsController : ControllerBase
-//     {
-//         private readonly IMediator _mediator;
-//         public CourseParticipationsController(IMediator mediator)
-//         {
-//             _mediator = mediator;
-//         }
+namespace its.gamify.api.Controllers
+{
+    [ApiController]
+    [Route("api/course-participations")]
+    public class CourseParticipationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CourseParticipationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
 
-//         [HttpGet]
-//         public async Task<IActionResult> GetAll([FromQuery] GetCourseParticipationQuery query)
-//         {
-//             var result = await _mediator.Send(query);
-//             if (result == null || result.Datas == null || !result.Datas.Any())
-//                 throw new InvalidOperationException("Danh sách CourseParticipation trống");
-//             return Ok(result);
-//         }
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetCourseParticipationQuery());
+            if (result == null || result.Datas == null || result.Datas.Count == 0)
+                throw new InvalidOperationException("Danh sách CourseParticipation trống");
+            return Ok(result);
+        }
 
-//         [HttpGet("{id}")]
-//         public async Task<IActionResult> GetById(Guid id)
-//         {
-//             var result = await _mediator.Send(new GetCourseParticipationByIdQuery { Id = id });
-//             if (result == null)
-//                 throw new InvalidOperationException($"Không tìm thấy CourseParticipation với id: {id}");
-//             return Ok(result);
-//         }
-//     }
-// }
+
+    }
+}
