13ab89feb71d6a5313d05f679a403f0316df9692
Quang-Thang <loquangthang07@gmail.com>
2025-07-19 02:39:17 +0700

    update: apply filter

--- src/its.gamify.core/Features/CourseResults/GetCourseResultByUserIdQuery.cs
+++ src/its.gamify.core/Features/CourseResults/GetCourseResultByUserIdQuery.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using its.gamify.core.Models.ShareModels;
 using its.gamify.domains.Entities;
 using MediatR;
@@ -14,10 +15,14 @@ namespace its.gamify.core.Features.CourseResults
 
             public async Task<BasePagingResponseModel<CourseResult>> Handle(GetCourseResultByUserIdQuery request, CancellationToken cancellationToken)
             {
+                Expression<Func<CourseResult, bool>> filter = x =>
+                    x.UserId == request.UserId &&
+                    (string.IsNullOrEmpty(request.FilterQuery.Q) || x.Course.Title.Contains(request.FilterQuery.Q!));
+
                 var (Pagination, Entities) = await _unitOfWork.CourseResultRepository.ToPagination(
                     pageIndex: request.FilterQuery.Page ?? 0,
                     pageSize: request.FilterQuery.Limit ?? 10,
-                    filter: x => x.UserId == request.UserId,
+                    filter: filter,
                     includes: [x => x.User, x => x.Course, x => x.CourseParticipation],
                     cancellationToken: cancellationToken);
                 return new BasePagingResponseModel<CourseResult>(Entities, Pagination);
