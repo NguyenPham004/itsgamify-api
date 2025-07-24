6bff17595f0a1b6a617f60eb6f951e1fb77df9ea
JTRerer <trongan147895@gmail.com>
2025-07-24 21:40:53 +0700

    update: create course

--- src/its.gamify.core/Features/Courses/Commands/CreateCourseCommand.cs
+++ src/its.gamify.core/Features/Courses/Commands/CreateCourseCommand.cs
@@ -20,39 +20,9 @@ namespace its.gamify.api.Features.Courses.Commands
                 RuleFor(x => x.CategoryId).NotNull().NotEmpty().WithMessage("Vui lòng nhập category id");
             }
         }
-        class CommandHandler : IRequestHandler<CreateCourseCommand, Course>
+        class CommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<CreateCourseCommand, Course>
         {
-            private readonly IUnitOfWork unitOfWork;
-            private readonly IFirebaseService firebaseService;
-            private readonly IMediator mediator;
-            public CommandHandler(IUnitOfWork unitOfWork,
-                IFirebaseService firebaseService,
-                IMediator mediator)
-            {
-                this.mediator = mediator;
-                this.firebaseService = firebaseService;
-                this.unitOfWork = unitOfWork;
 
-            }
-            private async Task<Quarter> UpsertQuarter(DateTime datetime)
-            {
-                var quater = await unitOfWork.QuarterRepository.FirstOrDefaultAsync(x => x.StartDate >= datetime && datetime <= x.EndDate);
-                var item = DateTimeUtilities.GetQuarterDates(datetime.Year, datetime.Month);
-                if (quater is null)
-                {
-                    // Create new 
-                    quater = new Quarter()
-                    {
-                        Name = $"Quý {(int)(datetime.Month / 4) + 1} {datetime.Year}",
-                        StartDate = item.StartDate,
-                        EndDate = item.EndDate,
-                        Year = datetime.Year
-                    };
-                    await unitOfWork.QuarterRepository.AddAsync(quater);
-                    await unitOfWork.SaveChangesAsync();
-                }
-                return quater;
-            }
             public async Task<Course> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
             {
                 var course = unitOfWork.Mapper.Map<Course>(request);
@@ -63,8 +33,6 @@ namespace its.gamify.api.Features.Courses.Commands
                     ?? throw new InvalidOperationException("Không tìm thấy Intro Video với Id " + request.IntroVideoId)).Url;
                 course.ThumbnailId = request.ThumbnailId;
                 course.IntroVideoId = request.IntroVideoId;
-                var quarter = await UpsertQuarter(DateTime.Now);
-                course.QuarterId = quarter.Id;
                 await unitOfWork.CourseRepository.AddAsync(course, cancellationToken);
                 await unitOfWork.SaveChangesAsync();
                 return course;
