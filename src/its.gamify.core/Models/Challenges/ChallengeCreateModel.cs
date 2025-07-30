a391bbfcbeee14a019db3151f6ad749789ff2b76
JTRerer <trongan147895@gmail.com>
2025-07-30 14:47:19 +0700

    update challenge

--- src/its.gamify.core/Models/Challenges/ChallengeCreateModel.cs
+++ src/its.gamify.core/Models/Challenges/ChallengeCreateModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace its.gamify.core.Models.Challenges
+﻿namespace its.gamify.core.Models.Challenges
 {
     public class ChallengeCreateModel
     {
@@ -13,5 +7,7 @@ namespace its.gamify.core.Models.Challenges
         public int NumOfRoom { get; set; }
         public string ThumbnailImage { get; set; } = string.Empty;
         public Guid CourseId { get; set; }
+        public Guid ThumbnailImageId { get; set; }
+        public Guid CategoryId { get; set; }
     }
 }
--- src/its.gamify.core/Models/Challenges/ChallengeUpdateModel.cs
+++ src/its.gamify.core/Models/Challenges/ChallengeUpdateModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace its.gamify.core.Models.Challenges
+﻿namespace its.gamify.core.Models.Challenges
 {
-    public class ChallengeUpdateModel:ChallengeCreateModel
+    public class ChallengeUpdateModel : ChallengeCreateModel
     {
     }
 }
