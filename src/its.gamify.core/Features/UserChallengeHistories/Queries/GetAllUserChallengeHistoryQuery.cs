e89c1cefe34e38d9a4cfa034ad7b9b0faf0dc106
JTRerer <trongan147895@gmail.com>
2025-08-21 22:33:15 +0700

    filter challenge title and status

--- src/its.gamify.core/Features/UserChallengeHistories/Queries/GetAllUserChallengeHistoryQuery.cs
+++ src/its.gamify.core/Features/UserChallengeHistories/Queries/GetAllUserChallengeHistoryQuery.cs
@@ -2,6 +2,7 @@
 using its.gamify.domains.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 
 namespace its.gamify.core.Features.UserChallengeHistories.Queries
@@ -15,11 +16,14 @@ namespace its.gamify.core.Features.UserChallengeHistories.Queries
 
             public async Task<BasePagingResponseModel<UserChallengeHistory>> Handle(GetAllUserChallengeHistoryQuery request, CancellationToken cancellationToken)
             {
+                Expression<Func<UserChallengeHistory, bool>> filter = x => x.UserId == request.UserId &&
+                (string.IsNullOrEmpty(request.Filter!.Q) || x.Status.Contains(request.Filter.Q) 
+                    || x.Challenge.Title.Contains(request.Filter.Q));
                 var (Pagination, Entities) = await unitOfWork.UserChallengeHistoryRepository
                 .ToDynamicPagination(pageIndex: request.Filter?.Page ?? 0,
                     pageSize: request.Filter?.Limit ?? 10,
-                    filter: x => x.UserId == request.UserId && (string.IsNullOrEmpty(request.Filter!.Q) || x.Status.ToString().Contains(request.Filter.Q, StringComparison.OrdinalIgnoreCase)),
-                    searchFields: ["ChallengeId", "UserId"], searchTerm: request.Filter?.Q ?? string.Empty,
+                    filter: filter,
+                    searchFields: ["ChallengeId", "UserId", "Status"], searchTerm: request.Filter?.Q ?? string.Empty,
                     sortOrders: request.Filter?.OrderBy?.ToDictionary(x => x.OrderColumn ?? string.Empty, x => x.OrderDir == "ASC"),
                     includeFunc: x => x.Include(x => x.Challenge).Include(x => x.User).Include(x => x.Winner));
                 return BasePagingResponseModel<UserChallengeHistory>.CreateInstance(Entities, Pagination);
